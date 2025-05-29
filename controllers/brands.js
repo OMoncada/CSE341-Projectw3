0213bd863fca11bccc83fc928019fd765da73cb8
OMoncada <132164886+OMoncada@users.noreply.github.com>
2025-05-29 11:04:40 -0400

    Update brands.js
    añadí try /catch

--- controllers/brands.js
+++ controllers/brands.js
@@ -1,34 +1,54 @@
 const Brand = require('../models/brand');
 const Api404Error = require('../middleware/api404Error');
 
-exports.getAll = async (req, res) => {
-  const brands = await Brand.find();
-  res.status(200).json(brands);
+exports.getAll = async (req, res, next) => {
+  try {
+    const brands = await Brand.find();
+    res.status(200).json(brands);
+  } catch (err) {
+    next(err); 
+  }
 };
 
-exports.getOne = async (req, res) => {
-  const brand = await Brand.findById(req.params.id);
-  if (!brand) throw new Api404Error(`Brand with ID ${req.params.id} not found`);
-  res.status(200).json(brand);
+exports.getOne = async (req, res, next) => {
+  try {
+    const brand = await Brand.findById(req.params.id);
+    if (!brand) throw new Api404Error(`Brand with ID ${req.params.id} not found`);
+    res.status(200).json(brand);
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.create = async (req, res) => {
-  const brand = new Brand(req.body);
-  const newBrand = await brand.save();
-  res.status(201).json(newBrand);
+exports.create = async (req, res, next) => {
+  try {
+    const brand = new Brand(req.body);
+    const newBrand = await brand.save();
+    res.status(201).json(newBrand);
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.update = async (req, res) => {
-  const updated = await Brand.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true
-  });
-  if (!updated) throw new Api404Error(`Brand with ID ${req.params.id} not found`);
-  res.status(200).json(updated);
+exports.update = async (req, res, next) => {
+  try {
+    const updated = await Brand.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    if (!updated) throw new Api404Error(`Brand with ID ${req.params.id} not found`);
+    res.status(200).json(updated);
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.delete = async (req, res) => {
-  const deleted = await Brand.findByIdAndDelete(req.params.id);
-  if (!deleted) throw new Api404Error(`Brand with ID ${req.params.id} not found`);
-  res.status(200).json({ message: 'Brand deleted' });
+exports.delete = async (req, res, next) => {
+  try {
+    const deleted = await Brand.findByIdAndDelete(req.params.id);
+    if (!deleted) throw new Api404Error(`Brand with ID ${req.params.id} not found`);
+    res.status(200).json({ message: 'Brand deleted' });
+  } catch (err) {
+    next(err);
+  }
 };
